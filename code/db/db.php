5c765eaf9584607ed07714a7a494f2da3175fc12
gililemta <gilile@mta.ac.il>
2024-02-03 14:55:00 +0200

    fix db connection

--- code/db/db.php
+++ code/db/db.php
@@ -3,18 +3,18 @@ $server_name="localhost";
 $database_name="shachafpe_bookme"; 
 
 // Production
-/*
+
 $user_name="shachafpe_liorz";
 $password="nAhIp+7hEGE";
 $conn=new mysqli($server_name,$user_name,$password,$database_name);
 $conn->set_charset("utf8");
 if ($conn->connect_error){
     die("Connection failed: ".$conn->connect_error);
-}*/
+}
 
 // Dev
 
-$server_name = "mysql";
+/*$server_name = "mysql";
 $user_name = "root";
 $password = "root";
 $database_name="shachafpe_bookme"; 
@@ -47,5 +47,5 @@ if ($conn->connect_error) {
 } else {
     echo "Connected successfully";
 }
-
+*/
 ?>
